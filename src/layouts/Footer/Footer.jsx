3b8df163ba28be20233e8909c756959662b8a595
md-maheen-billah <md.maheen.billah@icloud.com>
2024-04-11 08:31:46 +0200

    added react leaflet and made changes to footer section

--- src/layouts/Footer/Footer.jsx
+++ src/layouts/Footer/Footer.jsx
@@ -1,9 +1,25 @@
+import { FaFacebook, FaLinkedin, FaInstagram } from "react-icons/fa";
+import { FaTree } from "react-icons/fa6";
 const Footer = () => {
   return (
     <div>
       <footer className="footer footer-center p-4 bg-[#05386B] text-[#5CDB95]">
         <aside>
+          <h2 className="flex items-center text-2xl gap-2">
+            Terra Scope <FaTree />
+          </h2>
           <p>Copyright © 2024 - All right reserved by Md. Maheen Billah</p>
+          <div className="text-xl mt-2 flex items-center gap-4">
+            <a href="https://www.facebook.com/md.maheen.billah.97/">
+              <FaFacebook />
+            </a>
+            <a href="https://www.linkedin.com/in/md-maheen-billah/">
+              <FaLinkedin />
+            </a>
+            <a href="https://www.instagram.com/md.maheen.billah">
+              <FaInstagram />
+            </a>
+          </div>
         </aside>
       </footer>
     </div>
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -6,6 +6,8 @@ import "aos/dist/aos.css";
 import { useEffect } from "react";
 import Marque from "../../components/Marque/Marque";
 import { Helmet } from "react-helmet-async";
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
 
 const Home = () => {
   useEffect(() => {
@@ -30,10 +32,37 @@ const Home = () => {
           Explore our curated estate list below and choose your ideal property
           from a diverse selection of options.
         </p>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 lg:gap-10 mt-4 lg:mt-10 mb-14">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 lg:gap-10 mt-4 lg:mt-10">
           {lands.map((land, idx) => (
             <Land land={land} key={idx}></Land>
           ))}
+        </div>
+        <div className="mt-8 lg:mt-28" data-aos="fade-up">
+          <h2
+            id="estate-s"
+            className="text-center text-[#05386B] font-bold text-3xl  animate__animated animate__headShake  animate__slow animate__infinite mb-4"
+          >
+            Look For Us
+          </h2>
+          <p className="text-center text-[#05386B]">
+            Below you will be able to see the current location of our
+            organization.
+          </p>
+        </div>
+        <div data-aos="fade-up" className="mt-4 lg:mt-10 mb-14">
+          <MapContainer
+            center={[50.892, 4.4182]}
+            zoom={13}
+            scrollWheelZoom={false}
+          >
+            <TileLayer
+              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <Marker position={[50.892, 4.4182]}>
+              <Popup>We are here!</Popup>
+            </Marker>
+          </MapContainer>
         </div>
       </div>
     </div>
